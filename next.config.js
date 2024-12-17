55ef23eae9ec157218a12e1de49396ce23ac95f7
RTSDA <av@rockvilletollandsda.org>
2024-12-16 22:04:15 -0500

    fix: simplify build process and remove all Vercel/Cloudflare adapters

--- next.config.js
+++ next.config.js
@@ -1,40 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: '.vercel/output/static',
+  output: 'standalone',
   images: {
     unoptimized: true,
-  },
-  webpack: (config, { isServer }) => {
-    // Split chunks into smaller sizes for Cloudflare's 25MB limit
-    config.optimization = {
-      ...config.optimization,
-      splitChunks: {
-        chunks: 'all',
-        maxInitialRequests: 25,
-        minSize: 20000,
-        maxSize: 20000000, // 20MB to be safe
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              // Get the package name
-              const match = module.context?.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/);
-              const packageName = match ? match[1] : 'vendors';
-              return `vendor.${packageName.replace('@', '')}`;
-            },
-            priority: 10,
-          },
-          common: {
-            minChunks: 2,
-            priority: -10,
-          },
-        },
-      },
-    };
-
-    return config;
-  },
+  }
 }
 
 module.exports = nextConfig
