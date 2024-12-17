f987f7c320d5c868e4287d7e37ceded31eb576fe
RTSDA <av@rockvilletollandsda.org>
2024-12-16 20:48:27 -0500

    fix: remove experimental CSS optimization and update Pages config

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   swcMinify: true,
-  experimental: {
-    optimizeCss: true,
-  },
   output: 'export',
   distDir: '.vercel/output/static',
   assetPrefix: '/',
